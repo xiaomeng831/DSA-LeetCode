c4a597f03eebbe28422ded48003b6e664230ca44
Brady <78976407+xiaomeng831@users.noreply.github.com>
2023-08-04 21:26:55 -0400

    Update 02_L20_ValidParentheses.cs

--- 03_Stack/02_L20_ValidParentheses.cs
+++ 03_Stack/02_L20_ValidParentheses.cs
@@ -21,19 +21,17 @@ public class Solution {
                 stack.Push(c);
             } 
             // ) ] } 时 -> 出栈并与 ) ] } 比较, 不相等时返回false
-            // ) ] } 时 -> 出栈
-                        //else if(注意两个条件的顺序)
+            // ) ] } 时 -> 出栈, 如果栈已经空了, 无法抵消, 返回false
+            // 注意 先判断 是否为空, 后进行比较, 因为比较一致, 会元素出栈, 栈会变空
             else if (stack.Count == 0 || stack.Pop() != pairs[c])
             {
                 return false;
             }
-            // else if(stack.Count == 0) 
-            // {
-            //     return false;
-            // }else if(stack.Pop() != pairs[c])
-            // {
-            //     return false;
-            // }
+
+            // if (条件1 || 条件2)
+            //      ↓↓  大部分情况下等价,为了代码简洁,可以经常考虑上面的写法
+            // if(条件1){} 
+            // else if(条件2){}
         }
 
         return stack.Count == 0;
